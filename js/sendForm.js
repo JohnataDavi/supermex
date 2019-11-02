ccee0b41ceea6e9e0337758bde054451584c2506
Johnata Davi <infojohnata@gmail.com>
2019-11-02 12:10:35 -0300

    js

---
+++ js/sendForm.js
@@ -0,0 +1,57 @@
+function sendEmail() {
+    var contato = "mailto:contato@supermex.com.br?";
+    var array = new Array();
+    var content = "";
+    var nome = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var telefone = document.getElementById('telefone').value;
+
+    var checkedBoxes = document.querySelectorAll('input[type=checkbox]:checked');
+    for (var i = 0; i < checkedBoxes.length; i++) {
+        array.push(checkedBoxes[i].value)
+    }
+
+
+    for (var i = 0; i < array.length; i++) {
+        console.log(array[i]);
+
+        var label = "label[for='" + array[i] + "']";
+        var labels = $(label);
+        var produto = labels[0].children[0].innerHTML;
+        content += produto + ": ";
+        //alert(produto);
+
+        var selectedValues = [];
+        var marca = "#marcas" + array[i] + " :selected";
+        $(marca).each(function () {
+            selectedValues.push($(this).val());
+            content += $(this).val() + " ";
+        });
+        //alert(selectedValues);
+        content += "\n";
+    }
+    //alert(content);
+
+    //document.location.href = "mailto:mailto:contato@supermex.com.br";
+
+    var subject = "subject=Lista de compras - "+nome+"&";
+    var body = "body=Nome: " + nome + " Telefone: \r\n " +telefone + " Email: \r\n " + email + " Lista de compras: \r\n " + content;
+    var email = contato + subject + body;
+    window.open(email);
+    //window.location.reload()
+    //document.getElementById("nextBtn").setAttribute("onclick", "nextPrev(1);");
+    $('html,body').scrollTop(0);
+
+}
+
+function ativarCmb(name) {
+    var queryCard = "div[name="+name+"]";
+    var query = "input[id="+name+"]";
+    var check = $(query);
+
+    if(check.is(':checked')){
+        check.prop('checked', false);
+    }else{
+        check.prop('checked', true);
+    }
+}
